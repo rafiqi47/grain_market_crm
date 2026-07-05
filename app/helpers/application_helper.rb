015145eefb0aac21950de0206a3ba5613d8b88e4
rafiqi47 <bilalahmedrafiqi@gmail.com>
2026-07-05 16:15:28 +0500

    custom button component

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def custom_button(color: "primary", variant: "contained", disabled: false, is_submit: false, full_width: false, &block)
+    content = capture(&block)
+    render(
+      "components/button",
+      color: color,
+      variant: variant,
+      disabled: disabled,
+      is_submit: is_submit,
+      full_width: full_width,
+      content: content
+    )
+  end
 end
