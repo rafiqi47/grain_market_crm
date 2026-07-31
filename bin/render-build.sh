3bc703ac7169b511a6f6fd36d4767d6857483726
rafiqi47 <bilalahmedrafiqi@gmail.com>
2026-07-31 12:05:14 +0500

    Add Render build script

---
+++ bin/render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+
+# Precompile assets (Propshaft + Tailwind + Importmap)
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
+# Run any pending migrations (primary + solid_cache + solid_queue + solid_cable)
+bundle exec rails db:migrate
